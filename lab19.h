337b3edfcb2f938a26470e927dfa9ce24633e496
ShibaNoi <evergreen19832@gmail.com>
2026-02-17 20:23:48 +0700

    lab19.h completed

--- lab19.h
+++ lab19.h
@@ -63,13 +63,45 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+	if(hp <= 0) return true;
+	else return false;
+}
 
+void Unit::guard(){
+	guard_on = true;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::beAttacked(int oppatk){
+	if(guard_on == false){
+		hp -= oppatk-def;
+		return oppatk-def;
+	}
+	else {
+		hp -= (oppatk-def)/3;
+		return (oppatk-def)/3;
+	}
+}
 
+int Unit::attack(Unit &target){
+	return target.beAttacked(atk);
+}
 
+int Unit::heal(){
+	int h = rand()%21+10;
+	if(hp != hpmax){
+	    if(hp+h <= hpmax){
+	        hp += h;
+	        return h;
+	    }
+	    else{
+	        h = hpmax-hp;
+	        hp = hpmax;
+	        return h;
+	    }
+	}
+	else return 0;
+}	    
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
